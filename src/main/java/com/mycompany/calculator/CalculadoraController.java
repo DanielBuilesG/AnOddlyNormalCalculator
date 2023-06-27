f22e08a1d87680b82d17b81f86d20092d298e25a
DanielBuilesG <daniel.builes99@gmail.com>
2023-06-27 11:51:51 -0500

    added all the oddly normal operations

--- src/main/java/com/mycompany/calculator/CalculadoraController.java
+++ src/main/java/com/mycompany/calculator/CalculadoraController.java
@@ -1,6 +1,7 @@
 package com.mycompany.calculator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,9 +15,37 @@ public class CalculadoraController {
     
     
     @GetMapping("/add")
-    public String add(@RequestParam(name="number", required=true, defaultValue="0")String valor){
+    public String add(@RequestParam(name="number", required=true, defaultValue="0") String valor, Model model){
         CurrentValue cv = CurrentValue.getInstance();
         cv.setCurrentValue(CurrentValue.getInstance().getCurrentValue() + Double.parseDouble(valor));
+        model.addAttribute("currval", cv.getCurrentValue());
+        
+        return "calculadora";
+    }
+    
+    @GetMapping("/multi")
+    public String multi(@RequestParam(name="number", required=true, defaultValue="0") String valor, Model model){
+        CurrentValue cv = CurrentValue.getInstance();
+        cv.setCurrentValue(CurrentValue.getInstance().getCurrentValue() * Double.parseDouble(valor));
+        model.addAttribute("currval", cv.getCurrentValue());
+        return "calculadora";
+    }
+    
+    @GetMapping("/substract")
+    public String substract(@RequestParam(name="number", required=true, defaultValue="0") String valor, Model model){
+        CurrentValue cv = CurrentValue.getInstance();
+        cv.setCurrentValue(CurrentValue.getInstance().getCurrentValue() - Double.parseDouble(valor));
+        model.addAttribute("currval", cv.getCurrentValue());
+        return "calculadora";
+    }
+    
+    
+    
+    @GetMapping("/divide")
+    public String divide(@RequestParam(name="number", required=true, defaultValue="0") String valor, Model model){
+        CurrentValue cv = CurrentValue.getInstance();
+        cv.setCurrentValue(CurrentValue.getInstance().getCurrentValue() / Double.parseDouble(valor));
+        model.addAttribute("currval", cv.getCurrentValue());
         return "calculadora";
     }
     
